3d75fa44370a266fde59b83c67aab20a4c829a48
cppHusky <cpphusky@outlook.com>
2024-06-27 12:32:59 +0800

    北京邮电大学2023-2024学年第二学期《高等数学A(下)期中考试试题(A卷)参考答案-cppHusky

--- clean.py
+++ clean.py
@@ -1,3 +1,5 @@
+# 请谨慎使用该脚本！
+# 需在完成一切编辑工作后运行，以请除archive中的临时文件和PDF文件。
 import os
 extensions = [
     "aux",
@@ -7,14 +9,14 @@ extensions = [
     "pdf",
     "synctex.gz"
 ]
-directory = 'archive'
-for ext in extensions:
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.endswith(f'.{ext}'):
-                file_path = os.path.join(root, file)
-                try:
-                    os.remove(file_path)
-                    print(f'Removed: {file_path}')
-                except OSError as e:
-                    print(f'Error: {file_path} : {e.strerror}')
+for directory in ['archive','LaTeX','Typst','Wolfram','Word']:
+    for ext in extensions:
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if file.endswith(f'.{ext}'):
+                    file_path = os.path.join(root, file)
+                    try:
+                        os.remove(file_path)
+                        print(f'Removed: {file_path}')
+                    except OSError as e:
+                        print(f'Error: {file_path} : {e.strerror}')
